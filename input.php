17b746071ca4f38a8b6a6f3b136a4f7e8f2ef9a4
Natthawut Suwannasri <natthawut.suw@ku.th>
2023-09-07 19:39:50 +0700

    add

---
+++ input.php
@@ -0,0 +1,13 @@
+<html>
+    <h3><body>
+        <form action ="result.php" method="get">
+            <label>ป้อนค่าที่ต้องการเเสดง:</label>
+            <input type="text" name="a" 
+                placeholder="ค่าที่ต้องการเเสดง" required/><br>
+            <label>จำนวนครั้งที่ต้องการเเสดง:</label>
+            <input type="text" name="b"
+                placeholder="จำนวนครั้ง" required/><br><br>
+           <input type="submit" value="ยืนยัน">
+        </form>    
+    </body></h3>
+</html>
---
+++ result.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $A = $_GET["a"];
+            $B = $_GET["b"];
+           for ($x=1; $B >= $x; $x++){
+            echo "<b>$A</b><br><br>";
+           }
+        ?>
+    </body>
+</html>
